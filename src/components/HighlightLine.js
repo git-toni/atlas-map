683c3b5f44c1711d988e661f3b925162778dbc02
Edgar Ordóñez <edrodriguez2310@gmail.com>
2018-10-06 13:43:41 +0200

    Added HighlightLine Component

--- src/components/HighlightLine.js
+++ src/components/HighlightLine.js
@@ -1,6 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 
+import { withData } from "./../context";
+import { FiAlertTriangle } from "react-icons/fi";
+
 const Wrapper = styled.div`
   width: 100%;
   height: 500px;
@@ -9,16 +12,23 @@ const Wrapper = styled.div`
   background-color: #FFFFFF;
   overflow-y: scroll;
 
-  .line-section:last-child {
+  .point-section:last-child {
     height: 100%;
   }
 `;
 
-const LineWrapper = styled.div`
+const PointWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+`;
+
+const ScheduleWrapper = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
 
-  .line-section:first-child {
+  .point-section:first-child {
     .rail-station {
       :before {
         height: 20px;
@@ -29,7 +39,7 @@ const LineWrapper = styled.div`
     }
   }
 
-  .line-section:last-child {
+  .point-section:last-child {
     .rail-station {
       height: 100%;
     }
@@ -58,7 +68,7 @@ const LineSection = styled.div`
 
 const Line = styled.div`
   width: 10px;
-  height: 150px;
+  min-height: 80px;
   background-color: #e31713;
   position: relative;
 
@@ -84,63 +94,127 @@ const InfoWrapper = styled.div`
 
 const InfoSection = styled.div`
   display: flex;
-  flex-direction: row;
+  flex: 1;
+  flex-direction: column;
+  margin-left: 20px;
 `;
 
-const HighlightLine = () => (
+const ConnectionsWrapper = styled.div`
+  display: flex;
+  margin-top: 5px;
+
+  img {
+    height: 20px;
+    margin: 0 2px;
+  }
+
+  .alert {
+    width: 100%;
+    text-align: right;
+    animation: blink 1s infinite;
+  }
+
+  @keyframes blink {
+    0% {opacity: 0}
+    49%{opacity: 0}
+    50% {opacity: 1}
+  }
+`;
+
+const Alert = styled(FiAlertTriangle)`
+  stroke: red;
+  font-size: 1em;
+`;
+
+const AlertConstruction = styled(FiAlertTriangle)`
+  stroke: #ffeb00;
+  font-size: 1em;
+`;
+
+const HighlightLine = ({ data }) => (
   <Wrapper>
+    <ScheduleWrapper>
 
-    <LineWrapper>
-      <LineSection className="line-section">
-        <div className="info-time">
-          <h6>6:03 AM</h6>
-          <p>1 min</p>
-        </div>
-        <Line className="rail-station" />
-      </LineSection>
+      <PointWrapper className="point-section">
+        <LineSection className="line-section">
+          <div className="info-time">
+            <h6>6:03 AM</h6>
+            <p>1 min</p>
+          </div>
+          <Line className="rail-station" />
+        </LineSection>
+        <InfoSection>
+          <h2>Hospital de Bellvitge</h2>
+        </InfoSection>
+      </PointWrapper>
 
-      <LineSection className="line-section">
-        <div className="info-time">
-          <h6>6:04 AM</h6>
-          <p>3 min</p>
-        </div>
-        <Line className="rail-station" />
-      </LineSection>
-      
-      <LineSection className="line-section">
-        <div className="info-time">
-          <h6>6:07 AM</h6>
-          <p>2 min</p>
-        </div>
-        <Line className="rail-station" />
-      </LineSection>
-      
-      <LineSection className="line-section">
-        <div className="info-time">
-          <h6>6:09 AM</h6>
-          <p>3 min</p>
-        </div>
-        <Line className="rail-station" />
-      </LineSection>
-    </LineWrapper>
+      <PointWrapper className="point-section">
+        <LineSection className="line-section">
+          <div className="info-time">
+            <h6>6:04 AM</h6>
+            <p>3 min</p>
+          </div>
+          <Line className="rail-station" />
+        </LineSection>
+        <InfoSection>
+          <h2>Bellvitge</h2>
+          <ConnectionsWrapper>
+            <img src="./src/static/R60.svg" />
+            <img src="./src/static/R6.svg" />
+            <img src="./src/static/R50.svg" />
+            <img src="./src/static/R5.svg" />
+            <img src="./src/static/R5a.svg" />
+            {data.alert? (
+              <div className="alert">
+                <Alert />
+              </div>
+            ) : null}
+          </ConnectionsWrapper>
+        </InfoSection>
+      </PointWrapper>
 
-    <InfoWrapper>
-      <InfoSection>
-        <h2>Hospital de Bellvitge</h2>
-      </InfoSection>
-      <InfoSection>
-        <h2>Bellvitge</h2>
-      </InfoSection>
-      <InfoSection>
-        <h2>L’Hospitalet Av. Carrile</h2>
-      </InfoSection>
-      <InfoSection>
-        <h2>Rambla Just Oliveras</h2>
-      </InfoSection>
-    </InfoWrapper>
+      <PointWrapper className="point-section">
+        <LineSection className="line-section">
+          <div className="info-time">
+            <h6>6:07 AM</h6>
+            <p>2 min</p>
+          </div>
+          <Line className="rail-station" />
+        </LineSection>
+        <InfoSection>
+          <h2>L’Hospitalet Av. Carrilet</h2>
+          <ConnectionsWrapper>
+            <img src="./src/static/R60.svg" />
+            <img src="./src/static/R6.svg" />
+            <img src="./src/static/R50.svg" />
+            <img src="./src/static/R5.svg" />
+            <img src="./src/static/R5a.svg" />
+            <img src="./src/static/R7.svg" />
+            <img src="./src/static/S33.svg" />
+            <img src="./src/static/S4.svg" />
+            {data.alertConstruction? (
+              <div className="alert">
+                <AlertConstruction />
+              </div>
+            ) : null}
+          </ConnectionsWrapper>
+        </InfoSection>
+      </PointWrapper>
 
-
+      <PointWrapper className="point-section">
+        <LineSection className="line-section">
+          <div className="info-time">
+            <h6>6:09 AM</h6>
+            <p>3 min</p>
+          </div>
+          <Line className="rail-station" />
+        </LineSection>
+        <InfoSection>
+          <h2>Rambla Just Oliveras</h2>
+        </InfoSection>
+      </PointWrapper>
+    </ScheduleWrapper>
   </Wrapper>
 );
 
-export default HighlightLine;
+export default withData(HighlightLine);
