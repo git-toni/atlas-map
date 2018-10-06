95863c876d300428e7fa725acc05819dca27afbd
Adrià Compte <muniatu@gmail.com>
2018-10-06 14:35:56 +0200

    routing styles

--- src/Routing.js
+++ src/Routing.js
@@ -1,5 +1,76 @@
 import React from "react";
 import axios from "axios";
+import styled from "styled-components";
+
+const PanelContainer = styled.div`
+  margin-top: 48px;
+  padding: 10px;
+  background-color: #f0f0f0;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.3);
+
+  form {
+    padding: 10px 0;
+  }
+
+  label span {
+    background-color: red;
+    font-size: 16px;
+    font-weight: bold;
+    color: white;
+    padding: 10px 15px;
+    border-radius: 4px 0 0 4px;
+    min-width: 80px;
+    display: inline-block;
+  }
+
+  input {
+    padding: 10px 15px;
+    font-size: 16px;
+    border: none;
+    color: #787878;
+    display: inline-block;
+  }
+
+  .submit {
+    background: black;
+    color: white;
+    border-radius: 0 4px 4px 0;
+  }
+
+  .select {
+    width: 100%;
+    padding: 10px 0;
+    text-align: center;
+    margin-top: 10px;
+    margin-bottom: 15px;
+  }
+
+  .select select {
+    display: inline-block;
+    font-size: 18px;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    background: transparent;
+    border: none;
+    padding: 10px 15;
+  }
+
+  .button-container {
+    text-align: center;
+  }
+
+  .button {
+    background: red;
+    padding: 10px 15px;
+    border-radius: 4px;
+    display: inline-block;
+    color: white;
+    font-size: 16px;
+    font-weight: bold;
+    border: none;
+    margin: 10px 0 20px;
+  }
+`;
 
 export default class Routing extends React.Component {
   constructor() {
@@ -78,32 +149,37 @@ export default class Routing extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <h1>holaa routing</h1>
-        <form onSubmit={this.handlerSearchInput} name="from" style={{ textAlign: "center" }}>
-          <label htmlFor="from">
-            From:
-            <input
-              id="from"
-              name="from"
-              onChange={this.handleChange}
-              value={this.state.from.name}
-            />
-          </label>
-          <input type="submit" />
-        </form>
-        <form onSubmit={this.handlerSearchInput} name="to" style={{ textAlign: "center" }}>
-          <label htmlFor="to">
-            To:
-            <input id="to" name="to" onChange={this.handleChange} value={this.state.to.name} />
-          </label>
-          <input type="submit" />
-        </form>
-        <select onChange={this.handleSelect} value={this.state.profile}>
-          <option value="cycling">Cycling</option>
-          <option value="walking">Walking</option>
-          <option value="driving">Driving</option>
-        </select>
-        <button onClick={this.fetchRouting}>Dame mi ruta</button>
+        <PanelContainer>
+          <form onSubmit={this.handlerSearchInput} name="from" style={{ textAlign: "center" }}>
+            <label htmlFor="from">
+              <span>FROM</span>
+              <input
+                id="from"
+                name="from"
+                onChange={this.handleChange}
+                value={this.state.from.name}
+              />
+            </label>
+            <input className="submit" type="submit" />
+          </form>
+          <form onSubmit={this.handlerSearchInput} name="to" style={{ textAlign: "center" }}>
+            <label htmlFor="to">
+              <span>TO</span>
+              <input id="to" name="to" onChange={this.handleChange} value={this.state.to.name} />
+            </label>
+            <input className="submit" type="submit" />
+          </form>
+          <div className="select">
+            <select onChange={this.handleSelect} value={this.state.profile}>
+              <option value="cycling">Cycling</option>
+              <option value="walking">Walking</option>
+              <option value="driving">Driving</option>
+            </select>
+          </div>
+          <div className="button-container">
+            <button className="button" onClick={this.fetchRouting}>Calcula la ruta</button>
+          </div>
+        </PanelContainer>
       </React.Fragment>
     );
   }
